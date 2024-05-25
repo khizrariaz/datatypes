87628de81af4394ddfa8991df88ec21be6c02078
Khizra Riaz <khizrariaz@gmail.com>
2024-05-25 10:14:38 +0500

    class work

---
+++ datatype.ts
@@ -0,0 +1,24 @@
+// string
+let meeting : string = "Tuesday";
+ console.log(meeting)
+
+// number
+let a: number = 123;
+console.log(a);
+
+// boolean
+let isStudent: boolean = false;
+console.log(isStudent);
+
+// undefined and null can be the subtype of all datatype
+// null. null is empty
+let d : null = null;
+console.log(d);
+
+let number : undefined = undefined;
+console.log(number);
+
+
+let randomvalue: any = "https://github.com/panaverse";
+console.log(randomvalue);
+
---
+++ index.js
@@ -0,0 +1,15 @@
+var message = "Khizra";
+console.log(message);
+var message = "123";
+console.log(message);
+var personName = "Khizra";
+// good day khizra
+console.log("Good day, " + personName + "!");
+var personAge = 20;
+var finalAge = personAge + 5;
+console.log(finalAge);
+//  string concatenation
+var firstname = "Khizra";
+var secondname = "Riaz";
+var fullname = "Khizra" + " " + "Riaz";
+console.log(fullname);
