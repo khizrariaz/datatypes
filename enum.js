cb3854107603f09cc57c089d420ac6010b6fc517
Khizra Riaz <khizrariaz@gmail.com>
2024-05-27 14:19:41 +0500

    class work

--- enum.js
+++ enum.js
@@ -6,7 +6,26 @@ var Color;
     Color[Color["Blue"] = 2] = "Blue";
 })(Color || (Color = {}));
 ; //starts with 0 index
-var c = Color.Green;
+var c = Color["Green"]; //Color.Green
+console.log(c); //ans=1
+var laptop;
+(function (laptop) {
+    laptop[laptop["hp"] = 0] = "hp";
+    laptop[laptop["del"] = 13] = "del";
+    laptop[laptop["mac"] = 14] = "mac";
+})(laptop || (laptop = {}));
+var laptopName = laptop.mac;
+console.log(laptopName); //14 index
+//to get index no. type enum k name wali use hogi i.e latop
+var laptops;
+(function (laptops) {
+    laptops[laptops["hps"] = 0] = "hps";
+    laptops[laptops["dels"] = 1] = "dels";
+    laptops[laptops["macs"] = 2] = "macs";
+})(laptops || (laptops = {}));
+var laptopNames = laptops[2];
+console.log(laptopNames); //macs
+//index no p jo name h usko get krny lye type ka name likhna h string
 var Color1;
 (function (Color1) {
     Color1[Color1["Red"] = 1] = "Red";
@@ -26,6 +45,23 @@ var Color2;
 ; //can assign values to all
 var colorIndex = Color2["Blue"];
 console.log(colorIndex);
+// ans= 10
+var flowers;
+(function (flowers) {
+    flowers[flowers["rose"] = 10] = "rose";
+    flowers[flowers["lily"] = 11] = "lily";
+    flowers[flowers["jasmine"] = 12] = "jasmine";
+})(flowers || (flowers = {}));
+var index = flowers.jasmine;
+console.log(index); //12
+var animals;
+(function (animals) {
+    animals[animals["dog"] = 0] = "dog";
+    animals[animals["cat"] = 1] = "cat";
+    animals[animals["bat"] = 2] = "bat";
+})(animals || (animals = {}));
+var ani = animals[1];
+console.log(ani); //cat
 var pendingState = "LOADING" /* LoadingState.PENDING */;
 var completedState = "LOADED" /* LoadingState.COMPLETED */;
 console.log("LOADING" /* LoadingState.PENDING */ === "LOADING"); //true
@@ -41,3 +77,11 @@ var birdName = myBirds.sparrow; //ans=0 sparrow on 0 index
 console.log(birdName);
 var secbirdName = myBirds.parrot; //ans=2 parrot on 2 index
 console.log(secbirdName);
+var parfume;
+(function (parfume) {
+    parfume["strong"] = "blue lady";
+    parfume["mild"] = "mildew";
+    parfume["weak"] = "farnes";
+})(parfume || (parfume = {}));
+var par = parfume["strong"]; // parfume.strong
+console.log(par);
--- enum.ts
+++ enum.ts
@@ -1,7 +1,19 @@
 //enums=  lecture from sir zia repo
 
 enum Color {Red, Green, Blue};//starts with 0 index
-var c: Color = Color.Green;
+var c: Color = Color["Green"]; //Color.Green
+console.log(c); //ans=1
+
+enum laptop {hp, del =13, mac}
+let laptopName: laptop = laptop.mac
+console.log(laptopName); //14 index
+//to get index no. type enum k name wali use hogi i.e latop
+
+enum laptops{hps, dels, macs}
+let laptopNames : string = laptops[2]
+console.log(laptopNames); //macs
+//index no p jo name h usko get krny lye type ka name likhna h string
+
 
 enum Color1 {Red = 1, Green, Blue};
 var colorName: string = Color1[2];
@@ -13,6 +25,15 @@ var colorIndex = Color2["Blue"];
 console.log(colorIndex);
 
 // ans= 10
+
+enum flowers {rose= 10, lily, jasmine}
+let index: flowers = flowers.jasmine
+console.log(index); //12
+
+enum animals {dog, cat , bat}
+let ani : string= animals[1]
+console.log(ani); //cat
+
 
 const enum LoadingState {
     PENDING = "LOADING",
@@ -37,20 +58,27 @@ console.log(birdName);
 let secbirdName: myBirds = myBirds.parrot ; //ans=2 parrot on 2 index
 console.log(secbirdName);
 
+enum parfume{strong= "blue lady", mild= "mildew", weak= "farnes"}
+ let par: parfume= parfume["strong"]// parfume.strong
+    console.log(par);
+    //jab values deden to index no. nai aty values print hoti hen like "blue lady"
 
-const enum LogLevel {
-    ERROR,
-    WARN,
-    INFO,
-    DEBUG
-    }
-    //type logs = "ERROR" | "WARN" | "INFO" | "DEBUG"    
-    type logs = keyof typeof LogLevel;
+    const enum LogLevel {
+        ERROR,
+        WARN,
+        INFO,
+        DEBUG
+        }
+        //type logs = "ERROR" | "WARN" | "INFO" | "DEBUG"    
+        type logs = keyof typeof LogLevel;
+        
+        const enum trafficSignal {
+            RED = "Red",
+            YELLOW = "Yellow",
+            GREEN = "Green"
+        }
+        type light = keyof typeof trafficSignal
     
-    const enum trafficSignal {
-        RED = "Red",
-        YELLOW = "Yellow",
-        GREEN = "Green"
-    }
-    type light = keyof typeof trafficSignal
+
+
         
